eabdaa8a1b52dab21123c7798833778420604302
Valentino <valentino136@gmail.com>
2023-05-26 10:56:23 +0200

    added second level with 2 moveable joints

--- Assets/04_Scripts/MuscleMover.cs
+++ Assets/04_Scripts/MuscleMover.cs
@@ -8,6 +8,8 @@ public class MuscleMover : MonoBehaviour
     public Transform Target;
     public Transform MuscleSphere;
     public LayerMask layerMask;
+    public GameObject[] MoveableJoints;
+    public GameObject otherTarget;
     Vector3 targetPos;
 
     bool isHolding;
@@ -15,7 +17,10 @@ public class MuscleMover : MonoBehaviour
     void Start()
     {
         targetPos = transform.position;
+        MoveableJoints = FindObjectsOfType<GameObject>();
+        //MoveableJoints = System.Array.FindAll(MoveableJoints, obj => obj.layer == LayerMask.NameToLayer("Muscle"));
     }
+
 
     // Update is called once per frame
     void Update()
@@ -29,6 +34,8 @@ public class MuscleMover : MonoBehaviour
             Target.transform.Translate(0, h, 0);
             Target.transform.position = new Vector3(Target.transform.position.x, Mathf.Clamp(Target.transform.position.y, -0.3f, 1.5f), Target.transform.position.z);
 
+            print(gameObject.name);
+
 
             //mousePos.z = 1f;
             //mousePos.x = transform.position.x;
@@ -41,17 +48,52 @@ public class MuscleMover : MonoBehaviour
 
         if (Input.GetMouseButtonDown(0))
         {
-            Ray rayToCameraPos = new Ray(transform.position, Camera.main.transform.position - transform.position);
-            Vector3 dir = gameObject.transform.position - Camera.main.transform.position;
-            RaycastHit hitInfo = new RaycastHit();
-            if (Physics.Raycast(Camera.main.transform.position, dir, out hitInfo, 1000, layerMask))
+            //Ray rayToCameraPos = new Ray(transform.position, Camera.main.transform.position - transform.position);
+            //Vector3 dir = gameObject.transform.position - Camera.main.transform.position;
+            //RaycastHit hitInfo = new RaycastHit();
+            //if (Physics.Raycast(Camera.main.transform.position, dir, out hitInfo, 1000, layerMask))
+            //{
+            //    if (hitInfo.collider.tag == "Moveable")
+            //    { isHolding = true;
+            //        print(hitInfo.collider.name);
+            //    }
+
+
+            //}
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
             {
-                if (hitInfo.collider.tag == "Moveable")
+                Debug.Log("Raycast hit an object: " + hit.collider.gameObject.name);
+
+                if (hit.collider.gameObject.name == gameObject.name)
+                {
                     isHolding = true;
+
+
+                    //foreach (GameObject joint in MoveableJoints)
+                    //{
+                    //    if (joint != gameObject)
+                    //    {
+                    //        joint.GetComponent<MuscleMover>().enabled = false;
+                    //    }
+                    //}
+                }
             }
+
+
         }
         else if (Input.GetMouseButtonUp(0))
-        { isHolding = false; }
+        { 
+            isHolding = false;
+
+            //foreach (GameObject joint in MoveableJoints)
+            //{
+            //        joint.GetComponent<MuscleMover>().enabled = true;
+            //}
+        }
 
     }
     private void OnMouseDown()
