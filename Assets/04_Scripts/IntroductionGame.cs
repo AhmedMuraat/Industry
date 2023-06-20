6d4e90c48dcbba5e686191eb7306fa5c5df5eb16
Hazem <457440@student.fontys.nl>
2023-06-20 23:53:21 +0200

    Completed Implemention of introduction
    This introduction is only for the first level to let the players know the controls and the goal of the game

--- Assets/04_Scripts/IntroductionGame.cs
+++ Assets/04_Scripts/IntroductionGame.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class IntroductionGame : MonoBehaviour
 {
@@ -9,25 +10,59 @@ public class IntroductionGame : MonoBehaviour
 
     public GameObject introductionUI;
     public GameObject step1UI;
+    public GameObject step2UI;
+    public LayerMask layerMask;
+
+    bool StepOneDone;
+    bool StepTwoDone;
+    bool StepThreeDone;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        introductionUI.SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.touchCount > 0)
+        if (Input.touchCount > 0 && !StepOneDone)
         {
             introductionUI.SetActive(false);
 
             step1UI.SetActive(true);
+            StepOneDone = true;
+        }
+        else if (!StepTwoDone && StepOneDone)
+        {
+            Touch touch = Input.GetTouch(0);
+            Ray ray = Camera.main.ScreenPointToRay(touch.position);
+            RaycastHit hitInfo = new RaycastHit();
+            if (Physics.Raycast(ray, out hitInfo, 1000, layerMask))
+            {
+                Debug.Log("Raycast hit an object: " + hitInfo.collider.gameObject.name);
+
+                if (hitInfo.collider.tag == "Moveable")
+                {
+                    StepTwoDone = true;
+                    step1UI.SetActive(false);
+                    step2UI.SetActive(true);
+                }
+            }
         }
 
-        
 
+
+
+    }
+   
+    public void CheckStep3()
+    {
+
+        if (!StepThreeDone && StepTwoDone)
+        {
+            step2UI.SetActive(false);
+        }
     }
 }
